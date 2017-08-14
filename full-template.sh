587bc8f7828714a77cb99db9bbdf2958e47a2f69
Carl-Erik Kopseng <carlerik@gmail.com>
2017-08-14 11:29:59 +0200

    Add a general template that can be tweaked for individual use

---
+++ full-template.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+artefact="some-resulting-file.js"
+
+##
+build(){
+    # run compile step, Makefile, grunt, gulp, whatever, that results in a file
+    echo "we are producing files" > $artefact
+    
+    # check for existence of the produced file - totally optional
+    (( $? == 0 )) && [[ -e $artefact ]]
+}
+
+clean(){
+    rm $artefact
+    git reset --hard
+
+    # Maybe also add `git clean -fd`? Remember that this file then needs to be outside the repo ...
+}
+
+
+# the actual test: this can be whatever you like, as long as it exits with a non-zero exit code on error
+#Examples: see the sample `max-size` for a test that simply checks if a file exceeds some size threshold
+do_test(){
+    #node my-test-that-exits-with-non-zero-when-failing.js
+    (( 1 + 1 == 2 ))
+}
+
+build
+
+# Ignore the results of this test if the build step failed
+if (( $? != 0 ));then
+    echo "Build step erred! Skipping current commit to avoid false negatives ..."
+
+    # Returning 125 in a script is equivalent to doing a `git bisect skip` on the CLI
+    # See `git help bisect` for more info
+    exit 125
+fi
+
+do_test
+
+STATUS=$?
+
+if (( $STATUS == 0 )); then
+    echo "Test OK"
+else
+    echo "Test FAIL"
+fi
+
+exit $STATUS
+
