8182d17d8a8c0a5d14bc4aafec88ef580fe6fb2a
Carl-Erik Kopseng <carlerik@gmail.com>
2023-11-09 09:09:39 +0100

    Update template to make simpler to understand

--- full-template.sh
+++ full-template.sh
@@ -9,11 +9,58 @@
 # git bisect good 
 # git bisect run ../my-test-script.sh
 
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-artefact="some-resulting-file.js"
+#######################
+### CUSTOMIZE START ###
+# Customize the clean, build and do_test sections, as well as ARTEFACT (if relevant for the cleanup/test)
+#######################
+ARTEFACT="some-resulting-file.js"
+build(){
+    # run compile step, Makefile, grunt, gulp, whatever, that results in a file
+    echo "we are producing files" > $ARTEFACT
+    
+    # check for existence of the produced file - totally optional
+    (( $? == 0 )) && [[ -e $ARTEFACT ]]
+}
 
-# You do not need to customize this function: just customize the clean, build and do_test sections
+clean(){
+    [[ -e "$ARTEFACT" ]] && rm $ARTEFACT 
+    git reset --hard
+
+    # Maybe also add `git clean -fd`? Remember that this file then needs to be outside the repo ...
+    
+    # rm -r node_modules
+}
+
+
+# the actual test: this can be whatever you like, as long as it exits with a non-zero exit code on error
+#Examples: see the sample `max-size` for a test that simply checks if a file exceeds some size threshold
+do_test(){
+    #node my-test-that-exits-with-non-zero-when-failing.js
+    (( 1 + 1 == 2 ))
+}
+### CUSTOMIZE END ###
+
+
+
+####################################################################################
+########## DO NOT MODIFY BELOW #####################################################
+# You do not need to customize these functions: just customize the clean, build and do_test sections
+####################################################################################
+verify-correct-script-location(){
+    pushd "$SCRIPT_DIR"
+    if git status 2>/dev/null >/dev/null; then
+        echo "It seems the script resides in a Git directory ($SCRIPT_DIR)" 
+        echo "Move it outside the repo to avoid it interfering with the Git Bisect process."
+        exit 1
+    fi
+    popd
+}
+
 main(){
+    verify-correct-script-location
+
     clean
 
     build
@@ -40,29 +87,5 @@ main(){
     exit $STATUS
 }
 
-build(){
-    # run compile step, Makefile, grunt, gulp, whatever, that results in a file
-    echo "we are producing files" > $artefact
-    
-    # check for existence of the produced file - totally optional
-    (( $? == 0 )) && [[ -e $artefact ]]
-}
-
-clean(){
-    rm $artefact 
-    git reset --hard
-
-    # Maybe also add `git clean -fd`? Remember that this file then needs to be outside the repo ...
-    
-    # rm -r node_modules
-}
-
-
-# the actual test: this can be whatever you like, as long as it exits with a non-zero exit code on error
-#Examples: see the sample `max-size` for a test that simply checks if a file exceeds some size threshold
-do_test(){
-    #node my-test-that-exits-with-non-zero-when-failing.js
-    (( 1 + 1 == 2 ))
-}
 
 main
